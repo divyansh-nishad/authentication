58f13dcbfcb60199ebd332661633dac83155e984
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2023-03-03 18:36:37 +0530

    -added signout

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -5,10 +6,24 @@ import 'package:flutter/src/widgets/framework.dart';
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  void signUserOut() {
+    FirebaseAuth.instance.signOut();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Logged in!')),
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: signUserOut,
+            icon: const Icon(Icons.logout),
+          ),
+        ],
+      ),
+      body: const Center(
+        child: Text('Logged in!'),
+      ),
     );
   }
 }
